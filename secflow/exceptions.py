068ce855599272d4dd6d88a450d181b26da9e10d
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-05-21 13:09:15 +0600

    Create exceptions.py
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ secflow/exceptions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env -S python3 -OO
+# coding:utf8
+
+"""This file manage Exceptions raised by SecFlow API."""
+
+
+class SecflowException(Exception):
+    """Base class for SecFlow tools exceptions."""
+
+    pass
